051ee77585717d48a05964b8625ed9c2a216135a
Achu_Prasad <achuachu61973@gmail.com>
2024-02-08 12:35:50 +0530

    update

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -13,22 +14,30 @@ const Login = () => {
   }
 
   return (
-    <div className="flex justify-center items-center w-[400px] h-auto border-black bg-white bg-opacity-25 mx-[50px] my-[50px] p-5 rounded-[25px] shadow-md shadow-right shadow-gray-300">
+    <div className="flex flex-col justify-center items-center w-[400px] h-[350px] border-black bg-white bg-opacity-25 mx-[30px] my-[30px] p-2 rounded-[25px] shadow-md shadow-right shadow-gray-300">
       <form onSubmit={handleSubmit}>
-      <h2 className="order-1 flex justify-center font-bold text-3xl text-[#5D846D] mt-5 mb-7">Signin</h2>
+
+        <h2 className="order-1 flex justify-center font-bold text-3xl text-[#5D846D] mt-2 mb-5">
+          Signin
+        </h2>
+
         <div className="flex flex-col justify-start">
-        <label htmlFor="password" className="font-regular text-[#959B95]">email:</label>
+          <label htmlFor="password" className="font-regular text-[#959B95]">
+            email:
+          </label>
           <input
-            className="font-semibold text-white bg-[#91C1A7] rounded-lg py-2 px-4 mt-1 mb-4 focus:outline-none focus:border-green-500 w-[300px] h-[35px]"
+            className="font-semibold text-white bg-[#91C1A7] rounded-lg px-4 mt-1 mb-4 focus:outline-none focus:border-green-500 w-[300px] h-[35px]"
             type="text"
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
             }}
           />
-          <label htmlFor="password" className="font-regular text-[#959B95]">password:</label>
+          <label htmlFor="password" className="font-regular text-[#959B95]">
+            password:
+          </label>
           <input
-            className="font-semibold text-white bg-[#528C75] rounded-lg py-2 px-4 mt-1 mb-4 focus:outline-none focus:border-green-500 w-[300px] h-[35px]"
+            className="font-semibold text-white bg-[#528C75] rounded-lg px-4 mt-1 mb-4 focus:outline-none focus:border-green-500 w-[300px] h-[35px]"
             type="password"
             value={password}
             onChange={(e) => {
@@ -37,22 +46,27 @@ const Login = () => {
           />
         </div>
 
-        <div className="flex flex-row gap-4 justify-between w-auto h-auto my-2 ">
+        <div className="flex flex-row justify-between w-[300px] my-2 ">
           <button
-            className="bg-[#00D355] hover:bg-[#528C75] text-white font-bold py-2 px-4 rounded"
+            className="bg-[#00D355] hover:bg-[#528C75] text-white font-poppins font-bold py-2 px-4 rounded-md w-[100px]"
             type="submit"
           >
             Sign up
           </button>
 
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-[#00D355] hover:bg-[#528C75] text-white font-poppins font-bold py-2 px-4 rounded-md w-[100px]"
             type="submit"
           >
             Sign in
           </button>
         </div>
       </form>
+
+      <p className=" font-poppins font-lightbold text-[14px] flex justify-center my-4 text-[#959B95]">
+        Forget your password ? <span className="text-[#528C75]"> Reset it !</span>
+      </p>
+
     </div>
   );
 };
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import Login from '../components/Login'
-
+import React from "react";
+import Login from "../components/Login";
 
 function LoginPage() {
   return (
-    <div className = "bg-green-100 w-screen h-screen p-0 m-0 flex justify-center items-center">
-      <Login className=""/>
+    <div className="bg-white w-screen h-screen  m-0 p-0 flex justify-center items-center">
+      <div className="relative">
+        <div className="flex relative w-[550px] h-[550px] rounded-[40px] bg-[#528C75] mx-20 overflow-hidden">
+          <h1 className="absolute font-poppins font-bold text-white text-[60px]  leading-tight flex items-center mx-20 top-[150px] ">
+            do <br />
+            you know <br />
+            How to-do ?
+          </h1>
+
+          <div className="absolute w-[200px] h-[200px] rounded-full bg-white right-[-70px] top-[-50px] pointer-events-none opacity-50"></div>
+
+          <div className="absolute w-[150px] h-[150px] rounded-full  bg-white opacity-50 bottom-[-85px] left-[100px]"></div>
+        </div>
+      </div>
+
+      <div className="flex flex-col relative w-[550px] h-[550px] mx-20">
+        <Login className=" z-10" />
+        <img
+          className="absolute right-[50px] top-[-10px] z-[0] w-[150px] h-[150px]"
+          src="./src/assets/images/Asset 1.png"
+          alt="Todo app logo"
+        />
+        <h2 className="font-poppins font-bold text-[50px] text-[#528C75] leading-tight pl-5 pt-2">
+          do it.....<br></br> or leave it
+        </h2>
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
