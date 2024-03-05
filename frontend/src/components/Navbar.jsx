e0bdf8ef73fb15ded58f0b40e3f2fd312f87f8ad
Achu_Prasad <achuachu61973@gmail.com>
2024-03-05 13:17:20 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,8 +7,10 @@ function Navbar() {
     <div>
       <div className='bg-[#00D355] w-full h-[80px] flex flex-row flex-auto justify-between items-center'>
         <div className='font-poppins font-bold text-white text-[30px] mx-5'>todo.</div>
-        <button onClick={()=>{navigate('/login')}} className='bg-white text-black fontt-poppins font-semibold rounded-md w-[100px] h-[30px] mx-5'>Log out</button>
-        <button onClick={()=>{navigate('/Signup')}} className='bg-white text-black fontt-poppins font-semibold rounded-md w-[100px] h-[30px] mx-5'>Signup</button>
+        <div className='w-[300px] h-[40px] flex flex-row justify-between items-center'>
+          <button onClick={()=>{navigate('/login')}} className='bg-white text-black fontt-poppins font-semibold rounded-md w-[100px] h-[30px] mx-5'>Log out</button>
+          <button onClick={()=>{navigate('/Signup')}} className='bg-white text-black fontt-poppins font-semibold rounded-md w-[100px] h-[30px] mx-5'>Signup</button>
+        </div>
       </div>
     </div>
   )
